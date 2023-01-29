aeb763027e47e6610df97907e1d2008f755a82e9
maicol <maykolandres4@gmail.com>
2023-01-29 10:16:04 -0500

    edited footer

--- portfolio/components/Footer.tsx
+++ portfolio/components/Footer.tsx
@@ -2,15 +2,15 @@
 
 const Footer = () => {
   return (
-    <footer className="h-64 bg-red pt-10">
+    <footer className=" bg-app-menu py-10">
       <div className="w-10/12 mx-auto">
    
         <div className="md:flex justify-center md:justify-between text-center ">
-          <p className="font-playfair font-semibold text-2xl text-yellow">
-            JANE ESPER
+          <p className="font-playfair font-semibold text-xl text-white">
+            Maicol Hernandez
           </p>
-          <p className="font-playfair text-md text-yellow">
-            ©2022 ESPER. All Rights Reserved.
+          <p className="mt-5 text-md text-white">
+            ©2023 | All Rights Reserved.
           </p>
         </div>
       </div>

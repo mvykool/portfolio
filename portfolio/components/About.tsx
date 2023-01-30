8b3a463c3b6f2e17b65b76adc2cd8fa50ac65216
maicol <maykolandres4@gmail.com>
2023-01-29 19:41:59 -0500

    added about me section

--- portfolio/components/About.tsx
+++ portfolio/components/About.tsx
@@ -21,7 +21,7 @@ const titleEnd: string = "</about>"
 
   return (
     <>
-    <span className='text-app-green text-sm' >{title}</span>
+    <span className='text-app-green text-sm md:text-base' >{title}</span>
     <section id='about' className='md:flex md:justify-between md:items-center md:h-full gap-16 py-5'>
       
  
@@ -30,13 +30,13 @@ const titleEnd: string = "</about>"
     <div className="basis-3/5 z-10 mt-5 flex justify-center md:order-2">
       {isAboveLarge ? (
         <div
-          className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-20 
-          before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+          className="relative z-0 ml-52 before:absolute before:-top-10 before:-left-10 
+          before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-app-green before:z-[-1] "
         >
           <img
             alt="profile"
-            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-            src="assets/person-1.jpeg"
+            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] "
+            src="assets/person-3.jpeg"
           />
         </div>
       ) : (
@@ -61,23 +61,28 @@ const titleEnd: string = "</about>"
         }}
       >
         <div className='flex space-x-4 mx-auto justify-center'>
-        <p className="text-4xl font-playfair z-10 text-center md:text-start">
+        <p className="text-4xl md:text-5xl font-playfair z-10 text-center md:text-start">
           About
         </p>
-        <p className="text-4xl font-playfair z-10 text-center md:text-start text-app-green">
+        <p className="text-4xl md:text-5xl font-playfair z-10 text-center md:text-start text-app-green">
           Me
         </p>
         </div>
 
-        <p className="mt-10 mb-7 text-sm text-center md:text-start">
-          Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum
-          viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
+        <p className="mt-10 mb-7 text-sm md:text-base text-center md:text-start">
+        Hi there! I'm Maicol, and I'm a highly driven, front-end focused 
+         web developer, with emphasis in accessibility, and responsive
+         design.
+        </p>
+        <p className="mt-10 mb-7 text-sm md:text-base text-center md:text-start">
+        I enjoy building amazing UIs, working with APIs, learning new things all the time,
+         and keeping myself up to date with the new tendencies of web development.
         </p>
       </motion.div>
 
     </div>
   </section>
-   <span className='text-app-green text-sm' >{titleEnd}</span>
+   <span className='text-app-green text-sm md:text-base' >{titleEnd}</span>
    </>
   )
 }

6b5b848b48f6e3a9f43716742e9755f100f6b842
maicol <maykolandres4@gmail.com>
2023-02-01 03:46:26 -0500

    changed the hover scale

--- portfolio/components/SideLeft.tsx
+++ portfolio/components/SideLeft.tsx
@@ -18,16 +18,16 @@ const SideRight = () => {
       <TbBrandGithub className='text-gray-400 h-5 w-5 cursor-pointer hover:scale-110 transition duration-200' />
       </a>
       <a href='#contact'>
-      <TbBrandGmail className='text-gray-400 h-5 w-5 cursor-pointer hover:scale-110 transition duration-200' />
+      <TbBrandGmail className='text-gray-400 h-5 w-5 cursor-pointer hover:scale-125 transition duration-200' />
       </a>
       <a target="_blank" rel="noreferrer" href="https://wa.me/573015874874">
-      <TbBrandWhatsapp className='text-gray-400 h-5 w-5  text-2xl cursor-pointer hover:scale-110 transition duration-200' />
+      <TbBrandWhatsapp className='text-gray-400 h-5 w-5  text-2xl cursor-pointer hover:scale-125 transition duration-200' />
       </a>
       <a target="_blank" rel="noreferrer" href="https://discordapp.com/users/844721056211927041">
-      <TbBrandDiscord className='text-gray-400 h-5 w-5 cursor-pointer hover:scale-110 transition duration-200' />
+      <TbBrandDiscord className='text-gray-400 h-5 w-5 cursor-pointer hover:scale-125 transition duration-200' />
       </a>
       <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/maicol-hernandez/">
-      <TbBrandLinkedin className='text-gray-400 h-5 w-5 cursor-pointer hover:scale-110 transition duration-200' />
+      <TbBrandLinkedin className='text-gray-400 h-5 w-5 cursor-pointer hover:scale-125 transition duration-200' />
       </a>
     </div>
     </>  

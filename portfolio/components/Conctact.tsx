1f4618d9e4f2c35d0a9ac9ff0c3fa3caa8fb30bc
maicol <maykolandres4@gmail.com>
2023-02-01 03:16:25 -0500

    added  padding for md view

--- portfolio/components/Conctact.tsx
+++ portfolio/components/Conctact.tsx
@@ -96,7 +96,7 @@ setTimeout(() => {
        initial="hidden"
        whileInView="show"
        viewport={{ once: true }}
-      className='md:flex md:justify-center md:py-5 rounded-sm md:mx-20 px-5 bg-app-gray '>
+      className='md:flex md:justify-center md:py-5 rounded-sm md:mx-20 px-1 md:px-5 bg-app-gray '>
       <form ref={form} className='flex flex-col justify-center p-3 my-2 md:w-[30vw] relative' onSubmit={sendEmail}>
         <div className='flex flex-col md:flex-row md:m-3 justify-center'>
         <label className=' w-20 font-semibold rounded-md flex justify-center p-1 my-1  text-white'></label>

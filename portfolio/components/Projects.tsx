33ac735e5035c5d1d962002d955b990d130accbd
mvykool <maykolandres4@gmail.com>
2024-03-10 07:05:48 -0500

    fixed size of containers

--- portfolio/components/Projects.tsx
+++ portfolio/components/Projects.tsx
@@ -67,7 +67,7 @@ const titleEnd: string = "</projects>"
           hidden: { opacity: 0, x: -50 },
           visible: { opacity: 1, x: 0 },
         }}>
-              <div className="hover:scale-95 duration-200 transition bg-app-gray rounded-sm  p-3 relative md:h-[70vh]  cursor-pointer">
+              <div className="hover:scale-95 duration-200 transition bg-app-gray rounded-sm  p-3 relative md:h-[60vh]  cursor-pointer">
               <Image width={700} height={500} src="/project1.png" alt="airman" className="object-left object-cover rounded-sm  h-[40vh] md:h-[20vh]" />
               <p className="my-5 flex justify-center font-bold text-2xl">Airman-<span className="text-app-green">23</span> </p>
 
@@ -83,14 +83,14 @@ const titleEnd: string = "</projects>"
 
               <p className="text-sm mb-2 text-gray-300 mx-2">Fully functional shopping cart, <span className="text-app-green font-semibold">payments with Stripe</span>, authentication, blog posts, chat & email service.</p>
 
-              <div className="flex justify-center mt-5 md:absolute md:bottom-3 md:w-full">
+              <div className="flex justify-center mt-5 md:bottom-0 md:absolute md:w-full">
                 <a href="https://airman-23.vercel.app/" target="_blank" rel="noreferrer" className="flex items-center m-5 border-2 border-white rounded-sm justify-center md:text-sm py-1 px-2 hover:scale-125 duration-200 transition hover:bg-white hover:text-deep-blue">
                   Live
-                 <HiOutlineExternalLink className="mx-2"/>
+                    <HiOutlineExternalLink className="mx-2"/>
                 </a>
                 <a href="https://github.com/mvykool/Airman-23/tree/main/airman-23" target="_blank" rel="noreferrer" className="flex items-center m-5 border-2 border-white rounded-sm justify-center md:text-sm py-1 px-2 hover:scale-125 duration-200 transition hover:bg-white hover:text-deep-blue">
                   Code
-                 <TbBrandGithub className="mx-2"/>
+                    <TbBrandGithub className="mx-2"/>
                 </a>
               </div>
               </div>
@@ -99,7 +99,7 @@ const titleEnd: string = "</projects>"
            {/**project 2 */}
 
 
-           <motion.div
+      <motion.div
         initial="hidden"
         className="mt-10 "
         whileInView="visible"
@@ -109,7 +109,7 @@ const titleEnd: string = "</projects>"
           hidden: { opacity: 0, x: -50 },
           visible: { opacity: 1, x: 0 },
         }}>
-           <div className="hover:scale-95 duration-200 transition bg-app-gray rounded-sm p-3 relative md:h-[70vh]  cursor-pointer">
+           <div className="hover:scale-95 duration-200 transition bg-app-gray rounded-sm p-3 relative md:h-[60vh]  cursor-pointer">
            <Image width={700} height={500} src="/project2.png" alt="airman" className=" object-cover rounded-sm  object-left h-[40vh] md:h-[20vh] md:w-full" />
                <p className="my-5 flex justify-center font-bold text-2xl">Lib<span className="text-app-green">ello</span> </p>
 
@@ -125,7 +125,7 @@ const titleEnd: string = "</projects>"
 
               <p className="text-sm  mb-2 text-gray-300 mx-2">Totally responsive Web app, with functionaity to create, erase notes. Authentication, <span className="text-app-green font-semibold">and inlcudes six different themes.</span></p>
 
-              <div className="flex justify-center mt-5 md:absolute md:bottom-3 md:w-full">
+              <div className="flex justify-center mt-5 lute md:bottom-0 md:absolute md:w-full">
                 <a href="https://libello.netlify.app/" target="_blank" rel="noreferrer" className="flex items-center m-5 border-2 border-white rounded-sm justify-center md:text-sm py-1 px-2 hover:scale-125 duration-200 transition hover:bg-white hover:text-deep-blue">
                   Live
                  <HiOutlineExternalLink className="mx-2"/>
@@ -151,7 +151,7 @@ const titleEnd: string = "</projects>"
           hidden: { opacity: 0, x: -50 },
           visible: { opacity: 1, x: 0 },
         }}>
-             <div className="hover:scale-95 duration-200 transition  rounded-sm bg-app-gray p-3 relative md:h-[70vh]  cursor-pointer">
+             <div className="hover:scale-95 duration-200 transition  rounded-sm bg-app-gray p-3 relative md:h-[60vh]  cursor-pointer">
              <Image width={700} height={500} src="/project3.png" alt="airman" className=" object-cover rounded-sm object-right h-[40vh] md:h-[20vh]" />
                <p className="my-5 flex justify-center font-bold text-2xl">Oh M<span className="text-app-green">y Dog</span> </p>
 
@@ -166,7 +166,7 @@ const titleEnd: string = "</projects>"
 
               <p className="text-sm  mb-2 text-gray-300 mx-2">Responsive Website, <span className="text-app-green font-semibold">with soft color palette</span>, and smooth animations.</p>
 
-              <div className="flex justify-center mt-5 md:absolute md:bottom-3 md:w-full">
+              <div className="flex justify-center mt-5 lute md:bottom-3 md:absolute md:w-full">
                 <a href="https://oh-my-dog.netlify.app/" target="_blank" rel="noreferrer" className="flex items-center m-5 border-2 border-white rounded-sm justify-center md:text-sm py-1 px-2 hover:scale-125 duration-200 transition hover:bg-white hover:text-deep-blue">
                   Live
                  <HiOutlineExternalLink className="mx-2"/>
@@ -193,7 +193,7 @@ const titleEnd: string = "</projects>"
           hidden: { opacity: 0, x: -50 },
           visible: { opacity: 1, x: 0 },
         }}>
-            <div className="hover:scale-95 duration-200 transition  rounded-sm bg-app-gray p-3 relative md:h-[70vh]  cursor-pointer">
+            <div className="hover:scale-95 duration-200 transition  rounded-sm bg-app-gray p-3 relative md:h-[60vh]  cursor-pointer">
             <Image width={700} height={500} src="/open-sesamo.png" alt="airman" className=" object-fill rounded-sm  h-[40vh] md:h-[20vh]" />
               <p className="my-5 flex justify-center font-bold text-2xl gap-2">Open<span className="text-app-green">Sesamo</span> </p>
 
@@ -208,7 +208,7 @@ const titleEnd: string = "</projects>"
 
               <p className="text-sm  mb-2 text-gray-300 mx-2">Password encyption, login using Fireauth<span className="text-app-green font-semibold">, totally responsive, & secured</span></p>
 
-              <div className="flex justify-center mt-5 md:absolute md:bottom-3 md:w-full">
+              <div className="flex justify-center mt-5 lute md:bottom-3 md:absolute md:w-full">
                 <button className="flex items-center m-5 border-2 border-gray-400 cursor-not-allowed rounded-sm justify-center md:text-sm py-1 px-2  duration-200 transition bg-gray-500">
                   Development
                 </button>

2b28585a5f7ae4b71fe848cad8f561cd0f558e25
Maicol Hernandez <maykolandres4@gmail.com>
2024-12-16 04:59:20 -0500

    removed side components

--- portfolio/components/SideRight.tsx
+++ portfolio/components/SideRight.tsx
@@ -1,24 +1,8 @@
-import React from 'react'
+import React from "react";
 
 const SideRight = () => {
-  return (
-    <>
-    <div className='fixed bottom-[28%] right-[8%]'>
-    <img src="/blob4.svg" alt="" className='h-20 w-20' />
-  </div>
-  <div className='fixed top-[8%] right-[1%]'>
-    <img src="/blob5.svg" alt="" className='h-[30vh] w-[10vw]' />
-  </div>
+  return <></>;
+};
 
-    <div className=' fixed top-[55%] -right-12 '>
-      <a href="mailto:maykolandres4@gmail.com = Feedback&body = Message" >
-        <p className='text-gray-400 text-sm md:text-xs rotate-90 cursor-pointer hover:scale-110 transition duration-200'>
-        maykolandres4@gmail.com
-        </p>
-      </a>
-    </div>
-    </>
-  )
-}
+export default SideRight;
 
-export default SideRight

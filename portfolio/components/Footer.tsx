4077d6f9bb757df7bd73ddd1e7db35b381d96634
Maicol Hernandez <maykolandres4@gmail.com>
2024-12-23 08:21:23 -0500

    changed footer logo

--- portfolio/components/Footer.tsx
+++ portfolio/components/Footer.tsx
@@ -1,16 +1,18 @@
-
+import { Logo } from "@/app/components/ui/Logo";
 
 const Footer = () => {
   return (
     <footer className="  bg-app-gray mt-36 py-5 ">
       <div className="w-8/12 mx-auto">
-          <div className="md:flex justify-center md:items-center md:justify-evenly text-center ">
-            <img src="/logo.png" alt="log" className="h-10 w-10 hidden md:block" />
-            <p className="font-playfair text-base my-2 md:text-sm text-white">
+        <div className="md:flex justify-center md:items-center md:justify-evenly text-center ">
+          <div>
+            <Logo />
+          </div>
+          <p className="font-playfair text-base my-2 md:text-sm text-white">
             Made By <span className="text-app-green">Maicol Hernandez</span>
           </p>
           <p className="mt-0 text-sm text-gray-400">
-            ©2023 | All Rights Reserved.
+            ©2025 | All Rights Reserved.
           </p>
         </div>
       </div>
@@ -18,4 +20,5 @@ const Footer = () => {
   );
 };
 
-export default Footer; 
+export default Footer;
+
